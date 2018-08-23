a5f376fab0ff4e8733fe40e96f430463bd01d783
Hai <ngongochai112@gmail.com>
2018-08-23 19:37:14 +0700

    add usercontrol

--- QLBH/NumericTextBox/Properties/AssemblyInfo.cs
+++ QLBH/NumericTextBox/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e23d9b94-8d65-401a-96af-32b65685edb9")]
+[assembly: Guid("1963760c-f3bf-43f8-9d4f-7087fff2a9a8")]
 
 // Version information for an assembly consists of the following four values:
 //
