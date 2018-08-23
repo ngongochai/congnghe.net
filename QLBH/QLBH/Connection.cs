b248e4c6380bb9827cd27c9d85f7c649a71530ef
Hai <ngongochai112@gmail.com>
2018-08-23 20:25:58 +0700

    connection
    add connection

---
+++ QLBH/QLBH/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBH
+{
+    public class Connection
+    {
+        public int Check_Config()
+        {
+            if (Properties.Settings.Default.conn == string.Empty)
+                return 1;// Chuỗi cấu hình không tồn tại
+            SqlConnection _Sqlconn = new
+            SqlConnection(Properties.Settings.Default.LTWNCConn);
+            try
+            {
+                if (_Sqlconn.State == System.Data.ConnectionState.Closed)
+                    _Sqlconn.Open();
+                return 0;// Kết nối thành công chuỗi cấu hình hợp lệ
+            }
+            catch
+            {
+                return 2;// Chuỗi cấu hình không phù hợp.
+            }
+        }
+        
+    }
+}
